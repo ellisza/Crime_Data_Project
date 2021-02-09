b4fe4b3e004448412ff8e4922fd42b2fc776e65d
Wesley Dashner <wdashner11@gmail.com>
2021-02-08 17:39:02 -0700

    some more stuff

--- Project2.py
+++ Project2.py
@@ -1,5 +1,6 @@
 # %%
 import pandas as pd
+import seaborn as sns
 
 df = pd.read_csv('crime-housing-austin-2015.csv')
 df.head()
@@ -15,8 +16,31 @@ print(seen)
 
 
 # %%
-crime = 'Highest_NIBRS_UCR_Offense_Description'
-df[df.Highest_NIBRS_UCR_Offense_Description == 'Agg Assault']
-# %%
 # Graph crimes with x and y coordinates (which are close to transportation?)
 # Compare amount of crimes in zip codes with income/race
+# %%
+sns.scatterplot(x=agg.X_Coordinate, y=agg.Y_Coordinate)
+# %%
+from scipy.stats import ttest_ind
+
+crime = 'Highest_NIBRS_UCR_Offense_Description'
+df[df.Highest_NIBRS_UCR_Offense_Description == 'Agg Assault']
+agg = df[df.Highest_NIBRS_UCR_Offense_Description == 'Agg Assault']
+
+agg['Medianhouseholdincome'] = agg['Medianhouseholdincome'].str.replace('$', '').astype('float')
+zip_codes = agg.groupby(by=['Zip_Code_Crime']).agg({'Medianhouseholdincome': 'mean', 'Key': 'count'}).reset_index().dropna()
+sns.scatterplot(x=zip_codes['Medianhouseholdincome'], y=zip_codes['Key'])
+# %%
+from scipy.stats import ttest_ind
+from sklearn import preprocessing
+
+names = zip_codes.columns
+scaler = preprocessing.StandardScaler()
+scaled_df = scaler.fit_transform(zip_codes)
+scaled_df = pd.DataFrame(scaled_df, columns=names)
+
+
+sns.distplot(scaled_df['Medianhouseholdincome'], label='Median Household Income', hist=False)
+sns.distplot(scaled_df['Key'], label='Number of aggravated assaults', hist=False)
+ttest_ind(scaled_df['Medianhouseholdincome'], scaled_df['Key'])
+# %%

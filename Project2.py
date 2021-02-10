c8038caad83a27471cebdc1132e2ab7c8cbb8c60
Wesley Dashner <wdashner11@gmail.com>
2021-02-09 19:37:30 -0700

    few small labeling changes

--- Project2.py
+++ Project2.py
@@ -1,4 +1,5 @@
 # %%
+from matplotlib.pyplot import ylabel
 import pandas as pd
 import seaborn as sns
 
@@ -25,12 +26,12 @@ crime = 'Highest_NIBRS_UCR_Offense_Description'
 df[df.Highest_NIBRS_UCR_Offense_Description == 'Agg Assault']
 agg = df[df.Highest_NIBRS_UCR_Offense_Description == 'Agg Assault']
 # %%
-sns.scatterplot(x=agg.X_Coordinate, y=agg.Y_Coordinate)
+sns.scatterplot(x=agg.X_Coordinate, y=agg.Y_Coordinate, title='Map of Aggravated Assaults')
 # %%
 
 agg['Medianhouseholdincome'] = agg['Medianhouseholdincome'].str.replace('$', '').astype('float')
 zip_codes = agg.groupby(by=['Zip_Code_Crime']).agg({'Medianhouseholdincome': 'mean', 'Key': 'count'}).reset_index().dropna()
-sns.scatterplot(x=zip_codes['Medianhouseholdincome'], y=zip_codes['Key'])
+sns.scatterplot(x=zip_codes['Medianhouseholdincome'], y=zip_codes['Key'], ylabel = 'Aggravated Assaults', title='Aggravated Assaults in Areas by Median Household Income')
 # %%
 # Standardizing median household income vs amount of crimes
 from scipy.stats import ttest_ind
@@ -45,8 +46,11 @@ scaled_df = pd.DataFrame(scaled_df, columns=names)
 sns.distplot(scaled_df['Medianhouseholdincome'], label='Median Household Income', hist = False)
 
 #%% 
+import matplotlib.pylab as plt
+
 sns.distplot(scaled_df['Medianhouseholdincome'], label='Median Household Income', hist=False)
 sns.distplot(scaled_df['Key'], label='Number of aggravated assaults', hist=False)
+plt.legend()
 ttest_ind(scaled_df['Medianhouseholdincome'], scaled_df['Key'])
 
 scaled_df.corr(method='pearson')
@@ -66,6 +70,7 @@ sns.scatterplot(scaled_df)
 import datetime
 import time
 from scipy.stats import ttest_ind
+import matplotlib.pyplot as plt
 
 point = 50000
 poor = agg[agg['Medianhouseholdincome'] <= point]
@@ -76,8 +81,10 @@ for index, row in poor.iterrows():
 for index, row in rich.iterrows():
     value = float(time.mktime(datetime.datetime.strptime(row['Report_Date'], '%d-%b-%y').timetuple()))
     rich.at[index, 'Report_Date'] = value
-sns.distplot(poor['Report_Date'], label=f'Less than ${point}', hist=False)
-sns.distplot(rich['Report_Date'], label=f'More than ${point}', hist=False)
+sns.distplot(poor['Report_Date'], label=f'< ${point}')
+sns.distplot(rich['Report_Date'], label=f'> ${point}')
+plt.legend()
+
 ttest_ind(poor['Report_Date'], rich['Report_Date'])
 
 # %%
